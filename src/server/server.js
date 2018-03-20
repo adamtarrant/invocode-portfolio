aec63af95035df36c4679ac92831969a51c6bbd8
AdamTaz <adamtarrant@hotmail.com>
2018-03-20 18:39:18 +0000

    initial commit

---
+++ src/server/server.js
@@ -0,0 +1,7 @@
+const app = require('./app').app;
+const https = require('https');
+const config = process.env.NODE_ENV == 'production' ? require('./config/config_prod.js') : require('./config/config_dev.js');
+
+//Listening
+app.listen(config.port);
+//https.createServer(config.sslOptions, app).listen(config.HTTPSPort);
