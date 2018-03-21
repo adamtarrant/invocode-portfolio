57b7262589e5989bd5d41a46aee0a81034666256
AdamTaz <adamtarrant@hotmail.com>
2018-03-21 22:49:59 +0000

    wip - folder structure complete

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 //NPM and Node Modules
 const express = require('express');
 const path = require('path');
-const sass = require('node-sass');
+const sassMiddleware = require('node-sass-middleware');
 
 //Config
 const config = process.env.NODE_ENV == 'production' ? require('./config/config_prod.js') : require('./config/config_dev.js');
@@ -9,14 +9,36 @@ const config = process.env.NODE_ENV == 'production' ? require('./config/config_p
 //Init of express app
 const app = express();
 
+//Set view engine
+app.set('view engine', 'ejs');
+
 //Static middleware
-app.use(express.static(path.join(__dirname, '../../views')));
-/* app.use((err, req, res, next) => {
+app.use(sassMiddleware({
+        src: path.join(__dirname, '../client/'), 
+        dest: path.join(__dirname, '../../public'),
+        debug: true       
+    }));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('There was an error on the server');
-}); */
+});
+
 
 ///Route handlers
+
+app.get('/', (req,res) => {
+        res.render('pages/index', {}, (err, html) => {
+            if(err) {
+                console.error(err.stack);
+                res.status(500).send('There was an error on the server');
+            } else {
+                res.send(html);
+            }
+        });
+        
+
+});
 
 app.all('/*', (req, res) => {
     console.log("entered route");
