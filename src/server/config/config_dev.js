6a04d618d63944dff4b429018e23763d3a44cf0d
AdamTaz <adamtarrant@hotmail.com>
2018-03-30 19:56:06 +0100

    wip - added items to slider

--- src/server/config/config_dev.js
+++ src/server/config/config_dev.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path')
 
 module.exports= {
-    port: 8080,
+    port: 3000,
     HTTPSPort: 443,
     sslOptions: {
         key: fs.readFileSync(path.join(__dirname, './key.pem')),
