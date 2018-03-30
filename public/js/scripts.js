0ab2f4d2764481990412a2d9797de0140f442b21
AdamTaz <adamtarrant@hotmail.com>
2018-03-30 19:56:06 +0100

    wip - added items to slider

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -41,30 +41,23 @@ $('#fullpage').fullpage({
             infinite: false,
             speed: 300,
             slidesToShow: 3,
-            slidesToScroll: 4,
+            slidesToScroll: 3,
             responsive: [{
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
                         infinite: true,
                         dots: true
                     }
                 },
                 {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 480,
+                    breakpoint: 650,
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1
                     }
-                }
+                },
   ]
       });
       
