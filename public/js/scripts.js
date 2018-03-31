76fd441517188d8f80c78f6e3cea3e795ffb9309
AdamTaz <adamtarrant@hotmail.com>
2018-03-31 22:55:30 +0100

    wip - added devicons and portfolio

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -17,7 +17,7 @@ $('#fullpage').fullpage({
     css3: true,
     scrollingSpeed: 700,
     autoScrolling: true,
-    dragAndMove: true,
+    dragAndMove: false,
     fitToSection: true,
     fitToSectionDelay: 1000,
     scrollBar: true,
@@ -33,34 +33,80 @@ $('#fullpage').fullpage({
     parallax: false,
     parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
     sectionSelector: 'section',
+    verticalCentered: true
 });
 
-//initialises the slider carousel using the slick library
-      $('.slider').slick({
+
+
+// func declaration for setting number of rows - awaiting fix from slick.js
+function setNoSlideRows() {
+    console.log('setting number of rows...');
+    if ($(window).width() < 840 || $(window).height() < 700) {
+        return 1;
+    } else {
+        return 2;
+    }    
+}
+
+//func declaration for reinitializing slide carousel with new options - awaiting fix from slick.js
+function reCalcSlideRows() {
+    let newNoRows = setNoSlideRows();
+    if(newNoRows == $('.slider').slick('slickGetOption', 'rows')) {        
+        return;
+    } else {
+        $('.slider').slick('unslick');
+        $('.slider').slick(setupSlideOptions(newNoRows));
+    }
+}
+
+//setup slick options
+function setupSlideOptions(rows) {
+    let slideOptions = {
+        rows: rows,
         dots: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            responsive: [{
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        infinite: true,
-                        dots: true
-                    }
-                },
-                {
-                    breakpoint: 650,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                },
-  ]
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+            {
+                breakpoint: 840,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+]
+  };
+  return slideOptions;
+}
+
+//func declaration for init of the slider carousel using the slick library
+function initSlick() {
+      $('.slider').slick(setupSlideOptions(setNoSlideRows()));
+}
+
+// initialise Slick
+initSlick();
+
+//change rows in slide carousel on window resize
+    let timer;
+      $(window).resize(() => {
+        clearTimeout(timer);
+
+        timer = setTimeout(reCalcSlideRows, 1000);
+        console.log('resizing...');
+        
       });
-      
+
+
 // makes the forms labels appear and input background change when text is present
     $(".form-item").on("input propertychange",function(){
      let formField = $(this).attr('name');
