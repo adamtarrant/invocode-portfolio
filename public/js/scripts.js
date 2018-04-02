95d70f319185e000d1268993b40b130108fa3ed1
AdamTaz <adamtarrant@hotmail.com>
2018-04-02 19:32:25 +0100

    wip - adding lightbox

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,58 @@
+// func declaration for setting number of rows - awaiting fix from slick.js
+function setNoSlideRows() {
+    if ($(window).width() < 840 || $(window).height() < 700) {
+        return 1;
+    } else {
+        return 2;
+    }    
+}
+
+//func declaration for reinitializing slide carousel with new options - awaiting fix from slick.js
+function reCalcSlideRows() {
+    let newNoRows = setNoSlideRows();
+    if(newNoRows == $('.slider').slick('slickGetOption', 'rows')) {        
+        return;
+    } else {
+        $('.slider').slick('unslick');
+        $('.slider').slick(setupSlideOptions(newNoRows));
+    }
+}
+
+//setup slick options
+function setupSlideOptions(rows) {
+    let slideOptions = {
+        rows: rows,
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+            {
+                breakpoint: 840,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+]
+  };
+  return slideOptions;
+}
+
+//func declaration for init of the slider carousel using the slick library
+function initSlick() {
+      $('.slider').slick(setupSlideOptions(setNoSlideRows()));
+}
+
+
 
 $(document).ready(function(){
 
@@ -27,7 +82,7 @@ $('#fullpage').fullpage({
     scrollHorizontally: false,
     easing: 'easeInOutCubic',
     fadingEffect: false,
-    responsiveWidth: 750,
+    responsiveWidth: 655,
     responsiveHeight: 400,
     responsiveSlides: false,
     parallax: false,
@@ -36,97 +91,48 @@ $('#fullpage').fullpage({
     verticalCentered: true
 });
 
-
-
-// func declaration for setting number of rows - awaiting fix from slick.js
-function setNoSlideRows() {
-    console.log('setting number of rows...');
-    if ($(window).width() < 840 || $(window).height() < 700) {
-        return 1;
-    } else {
-        return 2;
-    }    
-}
-
-//func declaration for reinitializing slide carousel with new options - awaiting fix from slick.js
-function reCalcSlideRows() {
-    let newNoRows = setNoSlideRows();
-    if(newNoRows == $('.slider').slick('slickGetOption', 'rows')) {        
-        return;
-    } else {
-        $('.slider').slick('unslick');
-        $('.slider').slick(setupSlideOptions(newNoRows));
-    }
-}
-
-//setup slick options
-function setupSlideOptions(rows) {
-    let slideOptions = {
-        rows: rows,
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [{
-            breakpoint: 1600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-            {
-                breakpoint: 840,
-                settings: {
-                    infinite: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-]
-  };
-  return slideOptions;
-}
-
-//func declaration for init of the slider carousel using the slick library
-function initSlick() {
-      $('.slider').slick(setupSlideOptions(setNoSlideRows()));
-}
-
 // initialise Slick
 initSlick();
 
-//change rows in slide carousel on window resize
+//change rows and other options in slide carousel on window resize
     let timer;
       $(window).resize(() => {
         clearTimeout(timer);
-
-        timer = setTimeout(reCalcSlideRows, 1000);
-        console.log('resizing...');
-        
+        timer = setTimeout(reCalcSlideRows, 1000);       
       });
 
 
 // makes the forms labels appear and input background change when text is present
-    $(".form-item").on("input propertychange",function(){
+    $(".form-item").on("input propertychange", () => {
      let formField = $(this).attr('name');
       $("label[for="+formField+"]").toggleClass("label-visible",!! $(".form-item[name="+formField+"]").val());
       $(".form-item[name="+formField+"]").toggleClass("filled",!! $(".form-item[name="+formField+"]").val());
     });
 
 // makes hamburger button expand the nav menu
-    $(".menu-hamburger").on("click", function(){
+    $(".menu-hamburger").on("click", () => {
        $(".nav-list").toggleClass("visible"); 
     });
 
 
 //makes nav menu buttons move to header when not on home section
-    $(".home-section").bind("DOMSubtreeModified", function(e) {
+    $(".home-section").bind("DOMSubtreeModified", e => {
         if (!$(".home-section").hasClass("active")) {
         $("nav").removeClass("nav-home");
         } else {
         $("nav").addClass("nav-home"); 
         }
     });
+
+//make lightbox appear at correct portfolio item
+    $(".slider-item").on("click", () => {
+
+    });
+
+
+function showLightBox(position) {
+    $(".lightbox-container, .lightbox").addClass("active");
+        $.fn.fullpage.setAllowScrolling(false);
+    }
 
     });
