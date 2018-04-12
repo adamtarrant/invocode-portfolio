9e5df81ac637202b9db3bf1c09f4a5a8203b31a4
AdamTaz <adamtarrant@hotmail.com>
2018-04-12 19:44:27 +0100

    wip - first deployment

--- src/server/config/config_prod.js
+++ src/server/config/config_prod.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 
 module.exports= {
     port: process.env.PORT,
-    mongoUri: process.env.MONGODB_URI,
-    dbName: process.env.HISTORY_DB,
-    collectionName: "recent",
-    apiKey1: process.env.APIKEY1,
-    apiKey2: process.env.APIKEY2,
-    apiHost: "api.cognitive.microsoft.com",
-    apiEndPoint: "/bing/v7.0/images/search",
+    randomQuoteApiKey: process.env.RQ_API_KEY,
+    emailAuth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.PASS
+    },
 }
--- src/server/server.js
+++ src/server/server.js
@@ -3,5 +3,5 @@ const https = require('https');
 const config = process.env.NODE_ENV == 'production' ? require('./config/config_prod.js') : require('./config/config_dev.js');
 
 //Listening
-app.listen(config.port, '192.168.0.16');
+app.listen(config.port);
 //https.createServer(config.sslOptions, app).listen(config.HTTPSPort);
